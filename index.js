008551e0e0d2719e89f7a683aac10c2dc6a1d6ae
tangtientrung <tientrung3600@gmail.com>
2021-10-15 18:26:59 +0700

    ok

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
-  $("#no").click(function(){
-    var top = Math.floor((Math.random() * 100) + 1);
-    var left = Math.floor((Math.random() * 100) + 1);
-    $(this).css("top", top+"%");
-    $(this).css("left", left+"%");
-  });
+    $("#no").on("mouseover", function () {
+        var top = Math.floor((Math.random() * 100) + 1);
+        var left = Math.floor((Math.random() * 100) + 1);
+        $(this).css("top", top+"%");
+        $(this).css("left", left+"%");
+        $(this).css("position", "absolute");
+    });
+    $("#yes").click(function(){
+        $("#canvas").css('display','block');
+        $(this).css("top", 0);
+        $(this).css("left", 0);
+        $(this).css("position", "none");
+        $('body').css("background", "#000");
+        $('.envelope').css("display", "none");
+    });
 });
